f6dbd29d20cc7ce54b3ea4f645b2dc84c5995ef8
Alex Winter <bannedsaveplay@yandex.ru>
2025-06-20 01:25:46 +0300

    Добавьте файлы проекта.

---
+++ CalculatorConsole/Calculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorConsole
+{
+    public class Calculator(double a, double b)
+    {
+        private double a = a;
+
+        private double b = b;
+
+        public double Add()
+        {
+            return a + b;
+        }
+
+        public double Subtract()
+        {
+            return a - b;
+        }
+
+        public double Multiply()
+        {
+            return a * b;
+        }
+
+        public double Divide()
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero.");
+            }
+            return a / b;
+        }
+
+        public double Modulus()
+        {
+            return a % b;
+        }
+    }
+}
---
+++ CalculatorConsole/Program.cs
@@ -0,0 +1,59 @@
+﻿using CalculatorConsole;
+
+while (true)
+{
+    Console.WriteLine("Введите первое число: ");
+
+
+    string? input1 = Console.ReadLine();
+
+    if (input1 == null || !double.TryParse(input1, out double number1))
+    {
+        Console.WriteLine("Некорректный ввод. Пожалуйста, введите число.");
+        continue;
+    }
+
+    Console.WriteLine("Введите второе число: ");
+
+    string? input2 = Console.ReadLine();
+    if(input2 == null || !double.TryParse(input2, out double number2))
+    {
+        Console.WriteLine("Некорректный ввод. Пожалуйста, введите число.");
+        continue;
+    }
+
+    Calculator calculator = new(number1, number2);
+
+    Console.WriteLine("Выберите операцию: +, -, *, /, %");
+
+    string? operation = Console.ReadLine();
+
+    switch (operation)
+    {
+        case "+":
+            Console.WriteLine($"Результат: {calculator.Add()}");
+            break;
+        case "-":
+            Console.WriteLine($"Результат: {calculator.Subtract()}");
+            break;
+        case "*":
+            Console.WriteLine($"Результат: {calculator.Multiply()}");
+            break;
+        case "/":
+            try
+            {
+                Console.WriteLine($"Результат: {calculator.Divide()}");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            break;
+        case "%":
+            Console.WriteLine($"Результат: {calculator.Modulus()}");
+            break;
+        default:
+            Console.WriteLine("Некорректная операция. Пожалуйста, выберите +, -, *, / или %.");
+            break;
+    }
+}
