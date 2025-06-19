3de5d77d830e6099da57a49a7881e070227afd98
Alex Winter <bannedsaveplay@yandex.ru>
2025-06-20 01:51:06 +0300

    Добавлен класс CalculatorLog(логгирование) и вставлена в class program

---
+++ CalculatorConsole/CalculatorLog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorConsole
+{
+    public class CalculatorLog
+    {
+        private List<string> logEntries = [];
+
+        // This class is a placeholder for a logger element that can be used in the Calculator application.
+        public CalculatorLog() { }
+
+        public void AddLogEntry(string entry)
+        {
+            logEntries.Add(entry);
+        }
+
+        public void ShowHistory()
+        {
+            if (logEntries.Count == 0)
+            {
+                Console.WriteLine("Нет истории на данный момент.");
+                return;
+            }
+            Console.WriteLine("История калькулятора:");
+            foreach (var entry in logEntries)
+            {
+                Console.WriteLine(entry);
+            }
+        }
+
+    }
+}
--- CalculatorConsole/Program.cs
+++ CalculatorConsole/Program.cs
@@ -2,22 +2,34 @@
 
 while (true)
 {
+    CalculatorLog calculatorLog = new CalculatorLog();
+
     Console.WriteLine("Введите первое число: ");
 
     string? input1 = Console.ReadLine();
 
+    calculatorLog.AddLogEntry($"Введена строка: {input1}");
+
     if (input1 == null || !double.TryParse(input1, out double number1))
     {
         Console.WriteLine("Некорректный ввод. Пожалуйста, введите число.");
+
+        calculatorLog.AddLogEntry($"Введена строка с нечисловым значением: {input1}. ERROR.");
+
         continue;
     }
 
     Console.WriteLine("Введите второе число: ");
 
     string? input2 = Console.ReadLine();
-    if(input2 == null || !double.TryParse(input2, out double number2))
+
+    calculatorLog.AddLogEntry($"Введена строка: {input2}");
+    if (input2 == null || !double.TryParse(input2, out double number2))
     {
         Console.WriteLine("Некорректный ввод. Пожалуйста, введите число.");
+
+        calculatorLog.AddLogEntry($"Введена строка с нечисловым значением: {input2}. ERROR.");
+
         continue;
     }
 
@@ -27,32 +39,53 @@ while (true)
 
     string? operation = Console.ReadLine();
 
+    calculatorLog.AddLogEntry($"Введена строка операции: {operation}");
+
     switch (operation)
     {
         case "+":
             Console.WriteLine($"Результат: {calculator.Add()}");
+
+            calculatorLog.AddLogEntry($"Результат сложения: {calculator.Add()}");
+
             break;
         case "-":
             Console.WriteLine($"Результат: {calculator.Subtract()}");
+
+            calculatorLog.AddLogEntry($"Результат вычисления: {calculator.Add()}");
+
             break;
         case "*":
             Console.WriteLine($"Результат: {calculator.Multiply()}");
+
+            calculatorLog.AddLogEntry($"Результат умножения: {calculator.Multiply()}");
+
             break;
         case "/":
             try
             {
                 Console.WriteLine($"Результат: {calculator.Divide()}");
+
+                calculatorLog.AddLogEntry($"Результат деления: {calculator.Divide()}");
             }
             catch (DivideByZeroException ex)
             {
                 Console.WriteLine(ex.Message);
+
+                calculatorLog.AddLogEntry($"Ошибка: {ex.Message}");
             }
             break;
         case "%":
             Console.WriteLine($"Результат: {calculator.Modulus()}");
+
+            calculatorLog.AddLogEntry($"Результат процента: {calculator.Modulus()}");
+
             break;
         default:
             Console.WriteLine("Некорректная операция. Пожалуйста, выберите +, -, *, / или %.");
+
+            calculatorLog.AddLogEntry($"Некорректная операция: {operation}. ERROR.");
+
             break;
     }
 
